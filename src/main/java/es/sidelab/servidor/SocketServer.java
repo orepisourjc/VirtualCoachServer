965adb41db141b0414a03bb614464e334e32c205
Adrian <a.perezfernandez@alumnos.urjc.es>
2017-03-28 14:16:23 +0200

    add socket

--- src/main/java/es/sidelab/servidor/SocketServer.java
+++ src/main/java/es/sidelab/servidor/SocketServer.java
@@ -1,41 +1,64 @@
 package es.sidelab.servidor;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SocketServer {
-	private static int puerto=6969;
-	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SpringApplication.run(SocketServer.class, args);
+		System.out.println("Servidor arrancado");
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
 
-		try{
-		ServerSocket serverSocker= new ServerSocket(puerto);
-		Socket socket=serverSocker.accept();
-		BufferedReader br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		PrintWriter pw=new PrintWriter(socket.getOutputStream());
-		System.out.println("Server iniciado");
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication("VirtualCoach@outlook.es","VirtualCoach");
+				}
+		});
 		
-		while(true){
-			String line=br.readLine();
-			pw.println(line);
-			pw.flush();
-			
-		}
-		}catch(Exception e){
-			e.printStackTrace();
-		}finally{
-			
-		}
+		ServerSocket serverSocket;
+		System.out.println("SERVICIO INTERNO INICIADO");
+		try {
+			serverSocket = new ServerSocket (5555);
+			while(true){
+				Socket socket = serverSocket.accept();
+				BufferedReader leerCliente = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String linea = leerCliente.readLine();
+				Message message = new MimeMessage(session);
+				message.setFrom(new InternetAddress("salewebdad@gmail.com"));
+				message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(linea));
+				message.setSubject("Confirmacion SaleWeb");
+				message.setText("Gracias por comprar en SaleWeb");
 
+				Transport.send(message);
+
+				System.out.println("Done");
+			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 	}
-
 }
